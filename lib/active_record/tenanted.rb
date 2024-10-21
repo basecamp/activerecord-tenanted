cde3e4f4e0240f2611482ee0f11b1575c088122a
Mike Dalessio <mike@37signals.com>
2024-10-21 10:54:00 -0400

    bundler gem active_record-tenanted
    --no-exe --coc --no-ext --mit --test=minitest --ci=github --linter=rubocop

---
+++ lib/active_record/tenanted.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tenanted/version"
+
+module ActiveRecord
+  module Tenanted
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/active_record/test_tenanted.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ActiveRecord::TestTenanted < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ActiveRecord::Tenanted::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
