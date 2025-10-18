9066334ea3e613ad8772ae55511b21b7e5ab4276
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2025-10-18 12:19:20 -0300

    refactor: remove cross tenant configuration

--- test/unit/tenant_test.rb
+++ test/unit/tenant_test.rb
@@ -719,8 +719,7 @@ describe ActiveRecord::Tenanted::Tenant do
         ActiveRecord::Base.connection.add_column :announcements, :custom_tenant_id, :string
         ActiveRecord::Base.connection.add_column :announcements, :user_id, :integer
 
-        User.cross_tenant_config(tenant_column: :custom_tenant_id)
-        User.has_one :announcement, class_name: "Announcement"
+        User.has_one :announcement, class_name: "Announcement", tenant_key: :custom_tenant_id
         Announcement.belongs_to :user
       end
 
@@ -750,8 +749,7 @@ describe ActiveRecord::Tenanted::Tenant do
         ActiveRecord::Base.connection.add_column :announcements, :custom_tenant_id, :string
         ActiveRecord::Base.connection.add_column :announcements, :user_id, :integer
 
-        User.cross_tenant_config(tenant_column: :custom_tenant_id)
-        User.has_many :announcements, class_name: "Announcement"
+        User.has_many :announcements, class_name: "Announcement", tenant_key: :custom_tenant_id
         Announcement.belongs_to :user
       end
 
