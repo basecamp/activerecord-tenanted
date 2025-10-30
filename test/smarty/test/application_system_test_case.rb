94e6d868716f5f18c57e5ef9292ca53f541b7a7e
Andrew Markle <andrew.markle@hey.com>
2025-10-30 14:28:27 -0400

    Add chrome to devcontainer so that we can run integration tests in it

--- test/smarty/test/application_system_test_case.rb
+++ test/smarty/test/application_system_test_case.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
-Capybara.server = :puma, { Silent: true } # suppress server boot announcement
+Capybara.server = :puma, { Silent: true }
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  if ENV["CHROMEDRIVER_PATH"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
+      driver_option.add_argument("--disable-dev-shm-usage")
+      driver_option.add_argument("--no-sandbox")
+    end
+
+    Selenium::WebDriver::Chrome::Service.driver_path = ENV["CHROMEDRIVER_PATH"]
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
+      driver_option.add_argument("--disable-dev-shm-usage")
+      driver_option.add_argument("--no-sandbox")
+    end
+  end
 end
