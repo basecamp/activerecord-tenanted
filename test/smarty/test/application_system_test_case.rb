31fe774a4d02e4d3c594a20e3625dd97898b7dbc
Andrew Markle <andrew.markle@hey.com>
2025-11-05 13:48:04 -0500

    Simplify SystemTest setup

--- test/smarty/test/application_system_test_case.rb
+++ test/smarty/test/application_system_test_case.rb
@@ -1,19 +1,10 @@
 require "test_helper"
 
-Capybara.server = :puma, { Silent: true }
+Capybara.server = :puma, { Silent: true } # suppress server boot announcement
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV["CHROMEDRIVER_PATH"]
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
-      driver_option.add_argument("--disable-dev-shm-usage")
-      driver_option.add_argument("--no-sandbox")
-    end
-
-    Selenium::WebDriver::Chrome::Service.driver_path = ENV["CHROMEDRIVER_PATH"]
-  else
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
-      driver_option.add_argument("--disable-dev-shm-usage")
-      driver_option.add_argument("--no-sandbox")
-    end
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
+    driver_option.add_argument("--disable-dev-shm-usage")
+    driver_option.add_argument("--no-sandbox")
   end
 end
