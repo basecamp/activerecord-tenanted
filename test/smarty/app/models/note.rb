ed0539c12eac5e0d60caefa3456974b1dba4adf4
Andrew Markle <andrew.markle@hey.com>
2025-11-07 11:23:14 -0500

    Try explicitly busting the cache

--- test/smarty/app/models/note.rb
+++ test/smarty/app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
   has_one_attached :image
 
-  after_update_commit :broadcast_replace
+  after_update_commit :expire_cache_and_broadcast
 
   after_update_commit do
     NoteCheerioJob.perform_later(self) if needs_cheerio?
@@ -10,4 +10,10 @@ class Note < ApplicationRecord
   def needs_cheerio?
     !body.include?("Cheerio!")
   end
+
+  private
+    def expire_cache_and_broadcast
+      Rails.cache.delete(self)
+      broadcast_replace
+    end
 end
